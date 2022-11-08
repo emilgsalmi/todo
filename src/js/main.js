24030bd16f7827c8688437c4773b51a958d4cb99
Emil <emil.salmi@hotmail.se>
2022-11-08 16:56:23 +0100

    redone if/else

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,18 @@
 /* min function */
 function done (toDo){
-    let checkedContainer=document.querySelector(".checked")
-    
+    let checkedContainer=document.querySelector(".checked ul")
+    let container=document.querySelector(".container ul")
+
     if(toDo.classList.contains("theList__checked")) {
+        checkedContainer.removeChild(toDo);
         toDo.classList.remove("theList__checked")
         console.log("Du har ångrat din check av " + toDo.innerText);
-        checkedContainer.innerHTML+="<div class='checked__item'>Du har ångrat din check av " + toDo.innerText + "</div>"
+        container.appendChild(toDo);
     } else{
         toDo.classList.add("theList__checked")
         console.log("Du har checkat av " + toDo.innerText);
-        checkedContainer.innerHTML+="<div class='checked__item'>Du har checkat av " + toDo.innerText + "</div>"
+        container.removeChild(toDo);
+        checkedContainer.appendChild(toDo);
     }
 
     }
@@ -28,23 +31,32 @@ coffe = new toDo ("drinking coffé", 30),
 shower = new toDo ("taking a shower", 20),
 ];
 
-
+/* min localStorage */
 localStorage.setItem("theList", JSON.stringify(toDoList));
 let myList = localStorage.getItem("theList");
 let List = JSON.parse(myList);
 
 /* mina skapda HTMLtaggar */
+let toDoTitle = document.createElement("h3")
+let doneTitle = document.createElement("h3") 
 let container = document.createElement("div");
 let otherContainer= document.createElement("div")
 let aList = document.createElement("ul");
+let checkedList= document.createElement("ul");
 
 /* här är mina HTML klasser */
 otherContainer.className="checked";
 container.className="container";
 aList.className="aList";
+container.appendChild(toDoTitle);
+otherContainer.appendChild(doneTitle);
 container.appendChild(aList);
+otherContainer.appendChild(checkedList);
 document.body.appendChild(container);
 document.body.appendChild(otherContainer);
+
+toDoTitle.innerHTML="To Do";
+doneTitle.innerHTML="Done";
 
 /* min forloop för listan */
 for (let i= 0; i < List.length; i++){

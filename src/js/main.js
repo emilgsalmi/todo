a47ce9a54d074da2738ad97460dda315a8e412cc
Emil <emil.salmi@hotmail.se>
2022-11-08 15:41:11 +0100

    i hope i did right

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,26 @@
+/* min function */
 function done (toDo){
+    let checkedContainer=document.querySelector(".checked")
+    
     if(toDo.classList.contains("theList__checked")) {
         toDo.classList.remove("theList__checked")
-
+        console.log("Du har ångrat din check av " + toDo.innerText);
+        checkedContainer.innerHTML+="<div class='checked__item'>Du har ångrat din check av " + toDo.innerText + "</div>"
     } else{
         toDo.classList.add("theList__checked")
+        console.log("Du har checkat av " + toDo.innerText);
+        checkedContainer.innerHTML+="<div class='checked__item'>Du har checkat av " + toDo.innerText + "</div>"
     }
 
     }
-
+/* min klass */
 class toDo{
     constructor(whatToDo,timeToDo){
     this.whatToDo=whatToDo;
     this.timeToDo=timeToDo;
     };
 }
-
+/* mitt objekt */
 let toDoList = [
 phone = new toDo ("looking through phone", 10),
 brush = new toDo ("brush teeth", 10),
@@ -24,20 +30,23 @@ shower = new toDo ("taking a shower", 20),
 
 
 localStorage.setItem("theList", JSON.stringify(toDoList));
-
 let myList = localStorage.getItem("theList");
 let List = JSON.parse(myList);
 
-console.log(List);
-
+/* mina skapda HTMLtaggar */
 let container = document.createElement("div");
+let otherContainer= document.createElement("div")
 let aList = document.createElement("ul");
 
+/* här är mina HTML klasser */
+otherContainer.className="checked";
 container.className="container";
 aList.className="aList";
 container.appendChild(aList);
 document.body.appendChild(container);
+document.body.appendChild(otherContainer);
 
+/* min forloop för listan */
 for (let i= 0; i < List.length; i++){
     let rutine = document.createElement("li");
     let time = document.createElement("span")
@@ -52,9 +61,10 @@ for (let i= 0; i < List.length; i++){
     aList.appendChild(rutine);
     rutine.appendChild(time);    
 }
-
+/* mina valda listItems */
 let listItem = document.querySelectorAll(".theList__item")
 
+/* min forloop för att sätta klick händelse */
 for (let i= 0; i < listItem.length; i++){
     listItem[i].addEventListener("click", () => {
     done(listItem[i]);
